64a85434cce198efc9645ba1416656a05652d6bc
pkyriakou <38666400+pkyriakou@users.noreply.github.com>
2021-09-23 13:53:57 +0300

    Add files via upload
    Added some comments, fixed some formatting

--- Code/player.py
+++ Code/player.py
@@ -1,55 +1,45 @@
 import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
+
 from Networks import *
-from torch.autograd import Variable
+
 import sys
 sys.path.insert(0, './Tasks')
-from t1 import *
-from t2 import *
-from t3 import *
+from Tasks.t1 import *
+from Tasks.t2 import *
+from Tasks.t3 import *
 
 
-import random
-import numpy as np
-from torch import optim
+# mai train method which contains training loop
+def train(value_network, max_steps, task, r_seed, sample):
+	if task == 1:
+		Env = task1(r_seed+sample+1)
+	if task == 2:
+		Env = task2(r_seed+sample+1)
+	if task == 3:
+		Env = task3(r_seed+sample+1)
 
-
-def train(value_network,max_steps,task,path):
-	if task==1:
-		Env = task1()
-	if task==2:
-		Env = task2()
-	if task==3:
-		Env = task3()
-
-
-	steps=0
-	avg_reward=0
+	steps = 0
+	avg_reward = 0
 	state= torch.Tensor([Env.state])
-	while steps< max_steps:
-		steps+=1
+	while steps < max_steps:
+		steps += 1
 
 		# Take action a with ε-greedy policy based on Q(s, a; θ)
 		action= get_egreedy_action(state, value_network)
 
 		# Receive new state s′ and reward r
-		newObservation, reward= Env.step(action)
-		next_state= torch.Tensor([newObservation])
-		avg_reward+= (1/steps)*(reward-avg_reward)
+		new_observation, reward = Env.step(action)
+		next_state = torch.Tensor([new_observation])
+		avg_reward += (1/steps)*(reward-avg_reward)
 
 		state = next_state
 		
 	return avg_reward
 
-def get_egreedy_action(observation, Qmodel):
-	Qpreds=Qmodel.forward(observation).detach()
-	action = Qpreds.argmax()
+
+# computes and returns greedy action given the model and an observation
+def get_egreedy_action(observation, q_model):
+	q_preds = q_model.forward(observation).detach()
+	action = q_preds.argmax()
+
 	return action
-
-	
-
-
-
-
