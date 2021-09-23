bba60e480705037610e7df5205284bb6ca3f360d
pkyriakou <38666400+pkyriakou@users.noreply.github.com>
2021-09-23 11:53:37 +0300

    Add files via upload
    Fixed some bugs, cleaned up the code and added some comments

--- Code/visualize.py
+++ Code/visualize.py
@@ -2,63 +2,67 @@ import pylab as pl
 import sys
 import argparse
 import random
+import torch
+
 sys.path.insert(0, './Tasks')
 from Networks import *
 
 
-def get_egreedy_action(observation, Qmodel):
-    Qpreds=  Qmodel.forward(observation).detach()
-    action=  Qpreds.argmax()
-    return	action
+def get_egreedy_action(observation, q_model):
+    q_preds = q_model.forward(observation).detach()
+    action = q_preds.argmax()
+    return action
+
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--network', type=str) #linear or deep
-	parser.add_argument('--param', type=str, default='params_10')
-	parser.add_argument('--task', type=int, default=1) #task ID: 1,2 or 3
-	args=parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--network', type=str)  # linear or deep
+    parser.add_argument('--param', type=str, default='params_10')
+    parser.add_argument('--task', type=int, default=1)  # task ID: 1,2 or 3
+    args = parser.parse_args()
 
-	if args.task==3:
-		from t3 import task3
-		from task3 import state_size, num_action
-		Env=task3(True)
+    if args.task == 3:
+        from Tasks.t3 import task3
+        from Tasks.t3 import state_size, num_action
+        env = task3(render=True)
+    elif args.task == 2:
+        from t2 import task2
+        from t2 import state_size, num_action
+        env = task2(render=True)
+    elif args.task == 1:
+        from Tasks.t1 import task1
+        from Tasks.t1 import state_size, num_action
+        env = task1(render=True)
 
-	elif args.task==2:
-		from t2 import task2
-		from t2 import state_size, num_action
-		Env=task2(True)
+    # initialize agent VFA network that will be used for visualisation
+    if args.task == 3 and args.network == 'deep':
+        value_network = CNN(3)
+    elif args.network == 'linear':
+        value_network = Linear(state_size, num_action, 1)
+    elif args.network == 'deep':
+        value_network = Deep(state_size, num_action)
 
-	elif args.task==1:
-		from t1 import task1
-		from t1 import state_size, num_action
-		Env=task1(True)
+    # retrieve network parameters from path specified by the user
+    if args.network is not None:
+        value_network.load_state_dict(torch.load(args.param))
 
-	
-	if args.task==3 and args.network=='deep':
-		value_network= CNN(3)
-	elif args.network=='linear':
-		value_network= Linear(state_size,num_action)
-	elif args.network=='deep':
-		value_network= Deep(state_size,num_action)
+    # below we run 10,000 time-steps of the task for visualisation
+    steps = 0
+    avg_reward = 0
+    state = torch.Tensor([env.state])
+    while steps < 10000:
+        steps += 1
 
-	if args.network != None:
-		value_network.load_state_dict(torch.load(args.param))
+        if args.network is None:  # if no network is given, random actions are selected
+            action = random.randint(0, num_action - 1)
+        else:
+            action = get_egreedy_action(state, value_network)
 
-	steps=0
-	avg_reward=0
-	state=torch.Tensor([Env.state])
-	while steps< 10000:
-	    steps+=1
+        new_observation, reward = env.step(action)
+        next_state = torch.Tensor([new_observation])
+        avg_reward += (1 / steps) * (reward - avg_reward)
+        state = next_state
 
-	    if args.network==None:
-	    	action=random.randint(0,num_action-1)
-	    else:
-	    	action=get_egreedy_action(state,value_network)
 
-	    newObservation, reward=  Env.step(action)
-	    next_state=  torch.Tensor([newObservation])
-	    avg_reward+=(1/steps)*(reward-avg_reward)
-	    state  =  next_state
-
-if __name__ == "__main__" :
-	main()
+if __name__ == "__main__":
+    main()
